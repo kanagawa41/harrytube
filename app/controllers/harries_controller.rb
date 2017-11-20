ba7fee9fc657fc677761aad5a62696ebcc6c049d
kishiratakuya <kishiratakuya@team-lab.com>
2017-11-20 18:44:52 +0900

    [update]サイドバーのレイアウトを変更

--- app/controllers/harries_controller.rb
+++ app/controllers/harries_controller.rb
@@ -1,9 +1,14 @@
 class HarriesController < ApplicationController
-  layout "to_c"
+  layout "blog_home1"
+
   before_action :init_setting
 
   def index
     @posts = HarriesService.index
+    # FIXME: キチンとしたロジックに基づき取得する。
+    @recommend_post = Post.order(created_at: :desc).limit(4)
+    # FIXME: キチンとしたロジックに基づき取得する。
+    @ranking_post = Post.order(created_at: :desc).limit(4)
   end
 
   private
