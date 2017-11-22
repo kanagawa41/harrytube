738f9acc6d255f822255a2c8459c5d4ee04379c1
kishiratakuya <kishiratakuya@team-lab.com>
2017-11-22 13:50:04 +0900

    [update]toastrの表示位置を調整

--- app/assets/javascripts/common/my_common.js
+++ app/assets/javascripts/common/my_common.js
@@ -7,6 +7,24 @@ modules.commons = (function () {
 var module = {}
 
 module.init = function() {
+	toastr.options = {
+	  "closeButton": false,
+	  "debug": false,
+	  "newestOnTop": false,
+	  "progressBar": false,
+	  "positionClass": "toast-bottom-left",
+	  "preventDuplicates": false,
+	  "onclick": null,
+	  "showDuration": "300",
+	  "hideDuration": "1000",
+	  "timeOut": "5000",
+	  "extendedTimeOut": "1000",
+	  "showEasing": "swing",
+	  "hideEasing": "linear",
+	  "showMethod": "fadeIn",
+	  "hideMethod": "fadeOut"
+	}
+
   if($('p.notice').text().length > 0){
     toastr.info($('p.notice').text());
   }
